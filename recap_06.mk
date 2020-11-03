3fd4716320205b90defc81b6399ac14793415459
Magicturtle12 <turntopage12@gmail.com>
2020-11-02 20:56:03 -0500

    Update and rename recap_06.rtf to recap_06.mk

---
+++ recap_06.mk
@@ -0,0 +1,40 @@
+
+#  Episode 6 Outline
+## Sushinomics
+##### Price: $
+>over 7 Days
+##### Market Cap: $
+>over 7 Days 
+
+##### 24hr Trading volume(avg of last 7 days): ~$
+>over 7 Days down
+
+
+## News and Development
+
+### 
+##### 
+
+
+### 
+##### 
+
+* * *
+
+## Noteworthy Proposals
+### 
+##### Made by:
+
+### 
+##### Made by: 
+
+
+### 
+##### Made by:
+
+### 
+##### Made by: 
+
+***
+
+***
